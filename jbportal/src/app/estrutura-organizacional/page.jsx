343d4d9d9e7a2f67c807536c4fa65391b5bbc082
JPFerreira96 <juliojpf21@gmail.com>
2024-09-13 17:10:18 -0300

    Ajustes e implementações de funcionalidades no módulo de Estrutura Organizacional

--- jbportal/src/app/estrutura-organizacional/page.jsx
+++ jbportal/src/app/estrutura-organizacional/page.jsx
@@ -1,17 +1,24 @@
 'use client';
 import ModelPage from "../../components/ModelPage"
-import Image from 'next/image';
 import EstruturaOrganizacional from './estrutura';
+import Link from 'next/link';
 
 export default function estrutura() {
   return (
     <div>
-        <ModelPage
-        titulo={"ESTRUTURA ORGANIZACIONAL"}
-        subtitulo={"Íncio » ESTRUTURA ORGANIZACIONAL"}
-      >
-      <EstruturaOrganizacional />
-      </ModelPage>
-    </div>
+  <ModelPage
+    titulo={"ESTRUTURA ORGANIZACIONAL"}
+    subtitulo={
+      <span>
+        <Link href="/" className="text-white hover:underline hover:text-yellow-500 hover:brightness-125 transition-all">
+          Início
+        </Link>
+        &nbsp;» <span className="brightness-125">ESTRUTURA ORGANIZACIONAL</span>
+      </span>
+    }
+  >
+    <EstruturaOrganizacional />
+  </ModelPage>
+</div>
   );
 }
