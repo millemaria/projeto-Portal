c8c0d4211dbd77b091899d506a46a6e777597614
Jamille Maria <124792344+millemaria@users.noreply.github.com>
2024-09-13 17:09:21 -0300

    criação de concursos

---
+++ jbportal/src/app/concursos-e-selecoes/page.jsx
@@ -0,0 +1,17 @@
+'use client';
+import ModelPage from "../../components/ModelPage"
+import Concursos from "./concursos";
+// import EstruturaOrganizacional from './estrutura';
+
+export default function concursos() {
+  return (
+    <div>
+        <ModelPage
+        titulo={"CONCURSOS E SELEÇÕES"}
+        subtitulo={"Íncio » CONCURSOS E SELEÇÕES"}
+      >
+        <Concursos />  
+      </ModelPage>
+    </div>
+  );
+}
---
+++ jbportal/src/app/legislacao/legislacao.jsx
@@ -0,0 +1,15 @@
+export default function legislacao() {
+    return (
+      <div className="p-4">
+        <iframe
+          src="https://sgijaboatao.septemcompliance.com/workflow/wfFlow_Execute_Embed.aspx?c=dSBjStqLJ%2fEwGnlmb7a5reWSzSSZXx9tztS7f2GJhI5TqPav2yif%2flQEoRBkg2EXka%2fj12FgGkG3rolVXCbu6g%3d%3d"
+          width="100%"
+          height="800px"
+          className="border-none"
+          style={{ maxWidth: '100%', minHeight: '500px' }}
+          title="Embedded Content"
+        />
+      </div>
+    );
+  }
+  
