7776f78df0b9dd3da412d78b656fc3686606c7d6
Larissa Machado <larissadsmachado0@gmail.com>
2024-09-25 11:18:40 -0300

    a

--- jbportal/src/app/lei-aldir-blanc/aldirblanc.jsx
+++ jbportal/src/app/lei-aldir-blanc/aldirblanc.jsx
@@ -23,39 +23,39 @@ export default function Aldirblanc() {
                     <div>
                     <h2 className="text-center text-3xl font-bold mb-4 text-[#003476]">CADASTRO CULTURAL</h2>
                     <div className="space-y-4">
-                        <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold  rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2 ">CADASTRO CULTURAL DO JABOATÃO DOS GURARAPES - PESSOA FÍSICA</a>
+                        <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/12/CADASTTRO-CULTURAL-DO-JABOATAO-DOS-GUARARAPES-PESSOA-FISICA.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold  rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2 ">CADASTRO CULTURAL DO JABOATÃO DOS GURARAPES - PESSOA FÍSICA</a>
 
-                        <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2 ">CADASTRO CULTURAL DO JABOATÃO DOS GUARARAPES - PESSOA JURÍDICA</a>
+                        <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/12/CADASTRO-CULTURAL-DO-JABOATAO-DOS-GUARARAPES-PESSOA-JURIDICA-compactado.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2 ">CADASTRO CULTURAL DO JABOATÃO DOS GUARARAPES - PESSOA JURÍDICA</a>
 
-                        <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">HOMOLOGçÕ NOVO CADASTRO CULTURAL DO JABOATÃO DOS GUARARAPES</a>
+                        <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/12/HOMOLOGACAO-NOVO-CADASTRO-CULTURAL-DO-JABOATAO-DOS-GUARARAPES.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">HOMOLOGçÕ NOVO CADASTRO CULTURAL DO JABOATÃO DOS GUARARAPES</a>
 
                     </div>
                     <br/>
                     <h2 className="text-center text-3xl font-bold mb-4 text-[#003476]">EDITAIS</h2>
                         <div className="space-y-4">
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ERRATA EDITAIS 002, 003 E 005</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ERRATA EDITAIS 002, 003 E 005</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 002.2021 FRUIÇÃO, CRIAÇÃO, DIFUSÃO E FORMAÇÃO COMPLETO</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 002.2021 FRUIÇÃO, CRIAÇÃO, DIFUSÃO E FORMAÇÃO COMPLETO</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 001/2020 | SELEÇÃO FESTIVAIS COMPLETO</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 001/2020 | SELEÇÃO FESTIVAIS COMPLETO</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 004.2021 SELEÇÃO PARA MEMÓRIA VIVA COMPLETO</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 004.2021 SELEÇÃO PARA MEMÓRIA VIVA COMPLETO</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL 005.2021 TECNICOS COMPLETO</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL 005.2021 TECNICOS COMPLETO</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 001/2020 | SELEÇÃO PARA PRÊMIO DE MESTRES E MESTRAS E ENTIDADES CULTURAIS DE RELEVÂNCIA NA CULTURA DO MUNICÍPIO DO JABOATÃO DOS GUARARAPES-PE</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 001/2020 | SELEÇÃO PARA PRÊMIO DE MESTRES E MESTRAS E ENTIDADES CULTURAIS DE RELEVÂNCIA NA CULTURA DO MUNICÍPIO DO JABOATÃO DOS GUARARAPES-PE</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 002/2020 | CHAMAMENTO PÚBLICO PARA SELEÇÃO E RECEBIMENTO DE SUBSÍDIO PARA ESPAÇOS CULTURAIS</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 002/2020 | CHAMAMENTO PÚBLICO PARA SELEÇÃO E RECEBIMENTO DE SUBSÍDIO PARA ESPAÇOS CULTURAIS</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">PRORROGAÇÃO DE PRAZO PARA INSCRIÇÃO EDITAIS Nº 001 E Nº 0002 DE 2020 - PRÊMIOS E SUBSÍDIO</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">PRORROGAÇÃO DE PRAZO PARA INSCRIÇÃO EDITAIS Nº 001 E Nº 0002 DE 2020 - PRÊMIOS E SUBSÍDIO</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado do Edital 02_2021 Fruição, Criação, Difusão e Formação</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado do Edital 02_2021 Fruição, Criação, Difusão e Formação</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado do Edital 03_2021_Festivais</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado do Edital 03_2021_Festivais</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado do Edital 04_2021 Memória Viva</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado do Edital 04_2021 Memória Viva</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado do Edital 05_2021 Técnicos</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado do Edital 05_2021 Técnicos</a>
 
                         </div>
                     </div>
@@ -64,25 +64,25 @@ export default function Aldirblanc() {
                     <div>
                         <h2 className="text-center text-3xl font-bold mb-4 text-[#003476]">LEIS E DECRETOS</h2>
                         <div className="space-y-4">
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ESOLUÇÃO Nº 002/2021 - CONSELHO MUNICIPAL DE POLÍTICA CULTURAL DO JABOATÃO DOS GURARAPES - CMPC</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ESOLUÇÃO Nº 002/2021 - CONSELHO MUNICIPAL DE POLÍTICA CULTURAL DO JABOATÃO DOS GURARAPES - CMPC</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">LEI FEDERAL Nº 14.017 | 29-06-2020</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">LEI FEDERAL Nº 14.017 | 29-06-2020</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">REGULAMENTAÇÃO DO DECRETO Nº 10.464 | 17-08-2020</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">REGULAMENTAÇÃO DO DECRETO Nº 10.464 | 17-08-2020</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">01 DECRETO Nº 120, DE 06 DE OUTUBRO DE 2020 - CRÉDITO ADICIONAL SUPLEMENTAR ALDIR BLANC</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">01 DECRETO Nº 120, DE 06 DE OUTUBRO DE 2020 - CRÉDITO ADICIONAL SUPLEMENTAR ALDIR BLANC</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">DECRETO Nº 132.2020 - 23 DE OUTUBRO DE 2020</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">DECRETO Nº 132.2020 - 23 DE OUTUBRO DE 2020</a>
                         </div>
                         <br/>
                         <h2 className="text-center text-3xl font-bold mb-4 text-[#003476]">Planos</h2>
                         <div className="space-y-4">
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">PLANO DE AÇÃO DA LEI ALDIR BLANC</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">PLANO DE AÇÃO DA LEI ALDIR BLANC</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">PLANO DE TRABALHO DA LEI ALDIR BLANC</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">PLANO DE TRABALHO DA LEI ALDIR BLANC</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">NOMEAÇÃO DO COMITÊ GESTOR da Lei Aldir Blanc - DIÁRIO OFICIAL</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">NOMEAÇÃO DO COMITÊ GESTOR da Lei Aldir Blanc - DIÁRIO OFICIAL</a>
 
                             
                         </div>
@@ -92,10 +92,10 @@ export default function Aldirblanc() {
                     <div>
                         <h2 className="text-center text-3xl font-bold mb-4 text-[#003476]">ATAS</h2>
                         <div className="space-y-4">
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DELIBERATIVA LEI ALDIR BLANC - CMPC - JULHO DE 2020</a>
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DELIBERATIVA LEI ALDIR BLANC - CMPC - AGOSTO DE 2020</a>
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DELIBERATIVA LEI ALDIR BLANC - CMPC - SETEMBRO DE 2020</a>
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DA REUNIÃO EXTRAORDINÁRIA - OUTUBRO DE 2020</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DELIBERATIVA LEI ALDIR BLANC - CMPC - JULHO DE 2020</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DELIBERATIVA LEI ALDIR BLANC - CMPC - AGOSTO DE 2020</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DELIBERATIVA LEI ALDIR BLANC - CMPC - SETEMBRO DE 2020</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DA REUNIÃO EXTRAORDINÁRIA - OUTUBRO DE 2020</a>
                         </div>
 
                         <br/>
@@ -103,9 +103,9 @@ export default function Aldirblanc() {
                         <div className="space-y-4">
 
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DA COMISSÃO GESTORA DA LEI ALDIR BLANC</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DA COMISSÃO GESTORA DA LEI ALDIR BLANC</a>
 
-                            <a href="#" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado Final</a>
+                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado Final</a>
 
 
                         </div>
