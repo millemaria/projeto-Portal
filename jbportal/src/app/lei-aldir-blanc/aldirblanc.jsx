86aba0eefd826dc370f6422ebaec2f68ee04f8bc
Larissa Machado <larissadsmachado0@gmail.com>
2024-09-25 14:40:21 -0300

    paginaok

--- jbportal/src/app/lei-aldir-blanc/aldirblanc.jsx
+++ jbportal/src/app/lei-aldir-blanc/aldirblanc.jsx
@@ -33,29 +33,29 @@ export default function Aldirblanc() {
                     <br/>
                     <h2 className="text-center text-3xl font-bold mb-4 text-[#003476]">EDITAIS</h2>
                         <div className="space-y-4">
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ERRATA EDITAIS 002, 003 E 005</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2021/09/ERRATA-EDITAIS-002-003-E-005.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ERRATA EDITAIS 002, 003 E 005</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 002.2021 FRUIÇÃO, CRIAÇÃO, DIFUSÃO E FORMAÇÃO COMPLETO</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2021/12/Resultado-do-Edital-02_2021-Fruicao-Criacao-Difusao-e-Formacao.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 002.2021 FRUIÇÃO, CRIAÇÃO, DIFUSÃO E FORMAÇÃO COMPLETO</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 001/2020 | SELEÇÃO FESTIVAIS COMPLETO</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2021/09/EDITAL-No-003.2021-SELECAO-FESTIVAIS-COMPLETO.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 001/2020 | SELEÇÃO FESTIVAIS COMPLETO</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 004.2021 SELEÇÃO PARA MEMÓRIA VIVA COMPLETO</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2021/09/EDITAL-No-004.2021-SELECAO-PARA-MEMORIA-VIVA-COMPLETO.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 004.2021 SELEÇÃO PARA MEMÓRIA VIVA COMPLETO</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL 005.2021 TECNICOS COMPLETO</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2021/09/EDITAL-005.2021-TECNICOS-COMPLETO.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL 005.2021 TECNICOS COMPLETO</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 001/2020 | SELEÇÃO PARA PRÊMIO DE MESTRES E MESTRAS E ENTIDADES CULTURAIS DE RELEVÂNCIA NA CULTURA DO MUNICÍPIO DO JABOATÃO DOS GUARARAPES-PE</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/11/EDITAL-PREMIO-MESTRES-E-MESTRAS-E-ENTIDADES-CULTURAIS-DE-RELEVANCIA.zip" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 001/2020 | SELEÇÃO PARA PRÊMIO DE MESTRES E MESTRAS E ENTIDADES CULTURAIS DE RELEVÂNCIA NA CULTURA DO MUNICÍPIO DO JABOATÃO DOS GUARARAPES-PE</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 002/2020 | CHAMAMENTO PÚBLICO PARA SELEÇÃO E RECEBIMENTO DE SUBSÍDIO PARA ESPAÇOS CULTURAIS</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/11/EDITAL-DE-SUBSIDIOS.zip" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">EDITAL Nº 002/2020 | CHAMAMENTO PÚBLICO PARA SELEÇÃO E RECEBIMENTO DE SUBSÍDIO PARA ESPAÇOS CULTURAIS</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">PRORROGAÇÃO DE PRAZO PARA INSCRIÇÃO EDITAIS Nº 001 E Nº 0002 DE 2020 - PRÊMIOS E SUBSÍDIO</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/11/02-PRORROGACAO-DE-PRAZO-PARA-INSCRICAO-EDITAIS-No-001-E-No-0002-DE-2020-PREMIOS-E-SUBSIDIO.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">PRORROGAÇÃO DE PRAZO PARA INSCRIÇÃO EDITAIS Nº 001 E Nº 0002 DE 2020 - PRÊMIOS E SUBSÍDIO</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado do Edital 02_2021 Fruição, Criação, Difusão e Formação</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2021/12/Resultado-do-Edital-02_2021-Fruicao-Criacao-Difusao-e-Formacao.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado do Edital 02_2021 Fruição, Criação, Difusão e Formação</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado do Edital 03_2021_Festivais</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2021/12/Resultado-do-Edital-03_2021_Festivais.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado do Edital 03_2021_Festivais</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado do Edital 04_2021 Memória Viva</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2021/12/Resultado-do-Edital-04_2021-Memoria-Viva.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado do Edital 04_2021 Memória Viva</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado do Edital 05_2021 Técnicos</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2021/12/Resultado-do-Edital-05_2021-Tecnicos.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado do Edital 05_2021 Técnicos</a>
 
                         </div>
                     </div>
@@ -64,25 +64,25 @@ export default function Aldirblanc() {
                     <div>
                         <h2 className="text-center text-3xl font-bold mb-4 text-[#003476]">LEIS E DECRETOS</h2>
                         <div className="space-y-4">
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ESOLUÇÃO Nº 002/2021 - CONSELHO MUNICIPAL DE POLÍTICA CULTURAL DO JABOATÃO DOS GURARAPES - CMPC</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2021/09/PUBLICACAO-RESOLUCAO-No-02-2021DIARIO-OFICIAL-17-DE-SETEMBRO-DE-2021.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ESOLUÇÃO Nº 002/2021 - CONSELHO MUNICIPAL DE POLÍTICA CULTURAL DO JABOATÃO DOS GURARAPES - CMPC</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">LEI FEDERAL Nº 14.017 | 29-06-2020</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/10/01-DECRETO-ALDIR-BLANC.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">LEI FEDERAL Nº 14.017 | 29-06-2020</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">REGULAMENTAÇÃO DO DECRETO Nº 10.464 | 17-08-2020</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/10/03-regulamentacao-do-decreto-lei-aldir-blanc.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">REGULAMENTAÇÃO DO DECRETO Nº 10.464 | 17-08-2020</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">01 DECRETO Nº 120, DE 06 DE OUTUBRO DE 2020 - CRÉDITO ADICIONAL SUPLEMENTAR ALDIR BLANC</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/11/01-DECRETO-No-120-DE-06-DE-OUTUBRO-DE-2020-CREDITO-ADICIONAL-SUPLEMENTAR-ALDIR-BLANC.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">01 DECRETO Nº 120, DE 06 DE OUTUBRO DE 2020 - CRÉDITO ADICIONAL SUPLEMENTAR ALDIR BLANC</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">DECRETO Nº 132.2020 - 23 DE OUTUBRO DE 2020</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2021/02/DECRETO-No-132.2020-23-DE-OUTUBRO-DE-2020-1.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">DECRETO Nº 132.2020 - 23 DE OUTUBRO DE 2020</a>
                         </div>
                         <br/>
                         <h2 className="text-center text-3xl font-bold mb-4 text-[#003476]">Planos</h2>
                         <div className="space-y-4">
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">PLANO DE AÇÃO DA LEI ALDIR BLANC</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/10/04-PASSO-A-PASSO-PARA-IMPLEMENTACAO-DA-LEI-ALDIR-BLANC-GESTAO-PUBLICA.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">PLANO DE AÇÃO DA LEI ALDIR BLANC</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">PLANO DE TRABALHO DA LEI ALDIR BLANC</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/10/04-PLANO-DE-TRABALHO-E-ACOES-LEI-ALDIR-BLANC-MUNICIPIO-DO-JABOATAO-DOS-GUARARAPES-2020.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">PLANO DE TRABALHO DA LEI ALDIR BLANC</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">NOMEAÇÃO DO COMITÊ GESTOR da Lei Aldir Blanc - DIÁRIO OFICIAL</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/11/04-NOMEACAO-DO-COMITE-GESTOR-da-Lei-Aldir-Blanc-DIARIO-OFICIAL.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">NOMEAÇÃO DO COMITÊ GESTOR da Lei Aldir Blanc - DIÁRIO OFICIAL</a>
 
                             
                         </div>
@@ -92,20 +92,19 @@ export default function Aldirblanc() {
                     <div>
                         <h2 className="text-center text-3xl font-bold mb-4 text-[#003476]">ATAS</h2>
                         <div className="space-y-4">
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DELIBERATIVA LEI ALDIR BLANC - CMPC - JULHO DE 2020</a>
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DELIBERATIVA LEI ALDIR BLANC - CMPC - AGOSTO DE 2020</a>
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DELIBERATIVA LEI ALDIR BLANC - CMPC - SETEMBRO DE 2020</a>
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DA REUNIÃO EXTRAORDINÁRIA - OUTUBRO DE 2020</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/10/ATA-DA-REUNIAO-ORDINARIA-DO-MES-DE-JULHO-13-07-2020.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DELIBERATIVA LEI ALDIR BLANC - CMPC - JULHO DE 2020</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/10/ATA-DA-REUNIAO-ORDINARIA-DO-MES-DE-AGOSTO-10-08-2020.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DELIBERATIVA LEI ALDIR BLANC - CMPC - AGOSTO DE 2020</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/10/ATA-DA-REUNIAO-ORDINARIA-DO-MES-DE-SETEMBRO-09-09-2020-CORRIGIDA.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DELIBERATIVA LEI ALDIR BLANC - CMPC - SETEMBRO DE 2020</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/11/03-ATA-DA-REUNIAO-EXTRAORDINARIA-DO-MES-DE-OUTUBRO-28-10-2020-Assinantes-1.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DA REUNIÃO EXTRAORDINÁRIA - OUTUBRO DE 2020</a>
                         </div>
 
                         <br/>
                         <h2 className="text-center text-3xl font-bold mb-4 text-[#003476]">COMISSÃO GESTORA</h2>
                         <div className="space-y-4">
 
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2020/12/ATA-DA-COMISSAO-GESTORA-DA-LEI-ALDIR-BLANC-COM-TIMBRE-1TEXTO-FINAL.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DA COMISSÃO GESTORA DA LEI ALDIR BLANC</a>
 
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">ATA DA COMISSÃO GESTORA DA LEI ALDIR BLANC</a>
-
-                            <a href="#" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado Final</a>
+                            <a href="https://portaldatransparencia.jaboatao.pe.gov.br/wp-content/uploads/2021/06/PORTARIA-CONTEMPLADOS-LEI-ALDIR-BLANC-PREMIOS-E-SUBSIDIOS-TEXTO-FINAL-com-ausencia-dos-impugnados.pdf" target="_blank" className="w-full block p-3 px-5 bg-[#003476] text-white font-semibold rounded-lg text-center shadow-md hover:bg-[#1d4e8f] shadow-stone-600 border-white border-2">Resultado Final</a>
 
 
                         </div>
